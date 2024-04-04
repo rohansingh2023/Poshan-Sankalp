d5bfaf772c0833d37d6f457014821a2c72b046b6
Pooja kadam <kadampoo431@gmail.com>
2024-04-05 01:31:06 +0530

    analyze done

--- frontend/src/app/analyze/page.jsx
+++ frontend/src/app/analyze/page.jsx
@@ -1,143 +1,225 @@
-"use client"
-import React, { useState, useEffect } from "react";
-import { signIn, useSession, getSession } from "next-auth/react";
-// import Admin from "layouts/Admin.js";
-import DiseaseForm from "@/components/DiseaseForm";
+"use client";
+import React, { useState } from "react";
 import IndexNavbar from "@/components/Navbars/IndexNavbar";
+import { toast } from 'react-toastify';
+
 export default function Disease() {
-  const { data: session, status } = useSession();
-  console.log(session);
-  const [loading, setLoading] = useState(true);
+  const [file, setFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [imageSrc, setImageSrc] = useState(null);
+  const [analysisResult, setanalyzedData]= useState(null);
 
-  useEffect(() => {
-    const securePage = () => {
-      
-        setLoading(false);
+  const handleFileChange = (e) => {
+    if(analysisResult){
+      setanalyzedData(null);
+    }
     
-    };
-    securePage();
-  });
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+    toast.success("YOUR FILE SUCCCSESSFULLY UPLOADED", {
+      position: "top-center"
+    });
+    // Display the selected image preview (optional)
+  };
 
-  if (loading) {
-    return <h2 style={{ marginTop: 100, textAlign: "center" }}>LOADING...</h2>;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!file) return;
+
+    setLoading(true);
+
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const response = await fetch("http://127.0.0.1:5000/analyzeReports", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Analysis Result:", data);
+        setanalyzedData(data);
+        const targetDiv = document.getElementById("scroll-div");
+      targetDiv.scrollIntoView({ behavior: "smooth", block: "start" });
+        // Handle analysis result as needed
+      } else {
+        toast.warn("Failed to analyze", {
+          position: "top-center"
+        });
+      }
+    } catch (error) {
+      console.error("Error occurred during analysis:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const clear =()=>{
+    if(analysisResult){
+      setanalyzedData(null);
+    }
   }
   return (
-
-    <> 
-       <IndexNavbar fixed />
-    <section className="relative py-20">
-    <div
-      className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden "
-      style={{ transform: "translateZ(0)" }}
-    >
-      <svg
-        className="absolute bottom-0 overflow-hidden"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-        version="1.1"
-        viewBox="0 0 2560 100"
-        x="0"
-        y="0"
-      >
-        <polygon
-          className="text-white fill-current"
-          points="2560 0 2560 100 0 100"
-        ></polygon>
-      </svg>
-    </div>
-
-    <div className="container mx-auto px-4 mt-0">
-      <div className="items-center flex flex-wrap bg-white">
-        <div className="w-full md:w-5/12 ml-auto mt-0 mr-auto px-2">
-          <img
-            alt="..."
-            className="max-w-full rounded-lg shadow-lg"
-            src="https://cdn.dribbble.com/userupload/11743109/file/original-a3fe110e0b3037c286b80b033b476102.gif"
-          />
+    <>
+      <IndexNavbar fixed />
+      <section className="relative py-20">
+        <div
+          className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden "
+          style={{ transform: "translateZ(0)" }}
+        >
+          <svg
+            className="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              className="text-white fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
         </div>
-        <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
-          <div className="md:pr-12">
-            <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blueGray-500">
-              <i className="fas fa-rocket text-xl"></i>
+
+        <div className="container mx-auto px-4 mt-0">
+          <div className="items-center flex flex-wrap bg-white">
+            <div className="w-full md:w-5/12 ml-auto mt-0 mr-auto px-2">
+              <img
+                alt="..."
+                className="max-w-full rounded-lg shadow-lg"
+                src="https://cdn.dribbble.com/userupload/11743109/file/original-a3fe110e0b3037c286b80b033b476102.gif"
+              />
             </div>
-            <h3 className="text-3xl font-semibold text-blueGray-500">AI-Enhanced Report Analysis</h3>
-            <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-            The preorts are analyzed in a clear format, highlighting normal findings and flagging
-             abnormal levels. This facilitates prompt decision-making and guides 
-             appropriate actions for improved outcomes.
-            </p>
-            <ul className="list-none mt-6">
-              <li className="py-2">
-                <div className="flex items-center">
-                  <div>
-                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                      <i className="fas fa-fingerprint"></i>
-                    </span>
-                  </div>
-                  <div>
-                    <h4 className="text-blueGray-500">
-                    1. Upload Your Report
-                    </h4>
-                  </div>
+            <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+              <div className="md:pr-12">
+                <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blueGray-500">
+                  <i className="fas fa-rocket text-xl"></i>
                 </div>
-              </li>
-              <li className="py-2">
-                <div className="flex items-center">
-                  <div>
-                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                      <i className="fab fa-html5"></i>
-                    </span>
-                  </div>
-                  <div>
-                    <h4 className="text-blueGray-500">
-                    2. Uncover Hidden Patterns
-                    </h4>
-                  </div>
-                </div>
-              </li>
-              <li className="py-2">
-                <div className="flex items-center">
-                  <div>
-                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                      <i className="far fa-paper-plane"></i>
-                    </span>
-                  </div>
-                  <div>
-                    <h4 className="text-blueGray-500 mb-3">
-                    3. Get clear, actionable recommendations 
-                    </h4>
-                  </div>
-                </div>
-              </li>
-            </ul>
+                <h3 className="text-3xl font-semibold text-blueGray-500">
+                  AI-Enhanced Report Analysis
+                </h3>
+                <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+                  The preorts are analyzed in a clear format, highlighting
+                  normal findings and flagging abnormal levels. This facilitates
+                  prompt decision-making and guides appropriate actions for
+                  improved outcomes.
+                </p>
+                <ul className="list-none mt-6">
+                  <li className="py-2">
+                    <div className="flex items-center">
+                      <div>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
+                          <i className="fas fa-fingerprint"></i>
+                        </span>
+                      </div>
+                      <div>
+                        <h4 className="text-blueGray-500">
+                          1. Upload Your Report
+                        </h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="py-2">
+                    <div className="flex items-center">
+                      <div>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
+                          <i className="fab fa-html5"></i>
+                        </span>
+                      </div>
+                      <div>
+                        <h4 className="text-blueGray-500">
+                          2. Uncover Hidden Patterns
+                        </h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="py-2">
+                    <div className="flex items-center">
+                      <div>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
+                          <i className="far fa-paper-plane"></i>
+                        </span>
+                      </div>
+                      <div>
+                        <h4 className="text-blueGray-500 mb-3">
+                          3. Get clear, actionable recommendations
+                        </h4>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </section>
-    <div
-      title="Disease Detection"
-      Text="Upload Image to detect crop disease"
-      // image={session.user.image}
-    >
-      <div className="flex flex-wrap mt-4 justify-center">
-        <div className="w-full mb-12 xl:mb-0 px-4">
-          <DiseaseForm />
+        <div className="container mx-auto text-center px-4 mt-0">
+          {/* Your existing UI code */}
+          {/* ... */}
+          <form
+            onSubmit={handleSubmit}
+            className="w-full flex max-w-lg items-center justify-center flex-col mx-auto py-5"
+          >
+            <div className="flex w-full items-center justify-center bg-gray-200 rounded-lg p-6">
+              <label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue-700 rounded-lg shadow-lg tracking-wide uppercase border border-blue-700 cursor-pointer hover:bg-blue-700 hover:text-white">
+                <svg
+                  className="w-8 h-8"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v3h2a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h2V5zm4 5h2v3h4v-3h2l-5-5-5 5z"
+                  />
+                </svg>
+                <span className="mt-2 text-base leading-normal">
+                  Select a file
+                </span>
+                <input
+                  id="file"
+                  name="file"
+                  type="file"
+                  className="hidden"
+                  onChange={handleFileChange}
+                  required
+                />
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-6 rounded"
+              disabled={!file || loading}
+            >
+              {loading ? "Analyzing..." : "Analyze Report"}
+            </button>
+          </form>
+          <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-6 rounded"
+              onClick={clear}
+            >
+              Clear
+            </button>
         </div>
-      </div>
-    </div>
+        {/* Display analysis result if available */}
+        {analysisResult && (
+          <div className="container mx-auto px-4 mt-8 scroll-div">
+            <h3 className="text-2xl font-semibold mb-4">Analysis Result:</h3>
+            <div className="bg-gray-200 p-4 rounded-lg">
+              {Object.keys(analysisResult).map((question, index) => (
+                <div key={index} className="mb-4">
+                  <h4 className="text-lg font-semibold mb-2">{question}</h4>
+                  <p className="text-base">{analysisResult[question]}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </section>
     </>
   );
 }
-
-// export async function getServerSideProps(context) {
-//   const session = await getSession(context);
-//   let userId = null;
-
-//   return {
-//     props: {
-//       session,
-//       userId,
-//     },
-//   };
-// }
